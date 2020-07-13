d298ca561200037e602da0220ab4f61e5b69b551
Brandon Rose <rose.brandon.m@gmail.com>
2020-07-13 13:20:36 -0700

    Updated correlation checker and Ethiopia Refugees CSV

--- refugee-correlation.py
+++ refugee-correlation.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
         trend_check = False   
         while country_check == False:
             while trend_check == False:
-                print("Please select (1 or 2) a country of interest:\n\t1. Somalia\n\t2. South Sudan")
+                print("\nPlease select (1 or 2) a country of interest:\n\t1. Somalia\n\t2. South Sudan")
                 try:
                     country = int(input())
                     if country == 1:
@@ -56,21 +56,35 @@ if __name__ == "__main__":
                     print('Please select a valid country.')
 
                 if country_check:
-                    print("Please input a term of interest:")
+                    print("\nPlease input a term of interest:")
                     term = input()
 
-                    translated = translate(term, lang)
-                    print(f"{term} translated into {lang} is {translated}.")
-
-                    df_trend = get_trend(translated, geo)
-                    if df_trend.shape[0] == 0:
-                        print("Term has no associated trend. Please try another term.")
+                translation_check = False
+                print("\nWould you like to translate the term into the native language? Yes (y) or No (n)?")
+                while translation_check == False:
+                    translation = input()
+                    if translation.lower() in ['y','yes','n','no']:
+                        translation_check = True
                     else:
-                        trend_check = True
+                        print("Please enter a valid response (y or n)")
 
-        corr = get_corr(df_refugee, df_trend, translated)
+                if translation.lower() == 'y' or translation.lower() == 'yes':
+                    translated = translate(term, lang)
+                    print(f"\n{term} translated into {lang} is {translated}.")
+                    trend_term = translated
+                else:
+                    trend_term = term
+                
+                df_trend = get_trend(trend_term, geo)
 
-        print("Would you like to try another term? Yes (y) or No (n)?")
+                if df_trend.shape[0] == 0:
+                    print("\nTerm has no associated trend. Please try another term.")
+                else:
+                    trend_check = True
+
+        corr = get_corr(df_refugee, df_trend, trend_term)
+
+        print("\nWould you like to try another term? Yes (y) or No (n)?")
         retry = input()
         if retry.lower() == 'n' or retry.lower() == 'no':
             running = False
